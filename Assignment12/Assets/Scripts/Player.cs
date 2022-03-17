bdfa7b8b39a6ac5927d330e2cdf7f6557a25d0ac
PatrickPiazza <Patrick_Piazza@bloomfield.edu>
2022-03-17 10:33:56 -0400

    Steps 2, 3, & 4

---
+++ Assignment12/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    [SyncVar]
+    public Color color;
+	
+	public override void OnStartClient() 
+	{
+ 		gameObject.GetComponent<Renderer>().material.color = color;
+ 	}
+}
